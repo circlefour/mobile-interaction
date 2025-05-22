cd6e17de914cbf5cec42020ee950beae5825b2d7
circlefour <0100.circle@gmail.com>
2025-05-22 17:34:36 -0400

    basic electron app with connect and disconnect ui button

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+const { io } = require("socket.io-client");
 const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('node:path')
 
@@ -6,13 +7,39 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true
+      preload: path.join(__dirname, 'preload.js')
+      // if context isolation is true by default, i don't need to explicitly add it
     }
   })
 
   win.loadFile('index.html')
 }
+
+let socket = null
+
+function handleSockConn(event) {
+  socket = io('http://localhost:3000');
+  
+  socket.on('connect', () => {
+    console.log('connecting to server');
+    socket.emit('watcher', null);
+  });
+  
+  socket.on('chaos', (data) => {
+    console.log('chaos data:', data);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('socket disconnected');
+  });
+}
+
+function handleSockDiss(event){
+  if (!socket) return;
+  socket.disconnect();
+  socket = null;
+}
+
 
 app.whenReady().then(() => {
   createWindow()
@@ -23,7 +50,11 @@ app.whenReady().then(() => {
       createWindow()
     }
   })
+
+  ipcMain.on('sockcon', handleSockConn)
+  ipcMain.on('sockdis', handleSockDiss)
 })
+
 
 // necessary for windows and linux
 app.on('window-all-closed', () => {
--- app/renderer.js
+++ app/renderer.js
@@ -1,13 +1,11 @@
-const form = document.getElementById('oscForm');
+const btn = document.getElementById("start");
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const ip = document.getElementById('ip').value;
-  const port = parseInt(document.getElementById('port').value, 10);
-
-  if (window.api && ip && port) {
-    window.api.sendConfig({ip, port });
-  }else {
-    console.error('missing api bridge or invalid input');
+btn.addEventListener("click", () => {
+  if (btn.innerHTML == "connect") {
+    window.sockAPI.sockConn();
+    btn.innerHTML = "disconnect";
+  } else {
+    window.sockAPI.sockDiss();
+    btn.innerHTML = "connect";
   }
 });
